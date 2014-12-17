73a0a7c825bc0f267f15702ef8f800bbe129bc47
Bhavnit34 <bhavnitrulez291@hotmail.com>
2014-12-17 13:05:54 +0000

    Revert "Week selector should now work for firefox and chrome"
    This reverts commit 8e6541d3d9e1632ea23fe4b206ed0aaeaea801eb.

--- script.js
+++ script.js
@@ -24,86 +24,38 @@ var numRooms = 1;
 //---------------------
 $(document).ready(function()		// Execute all of this on load 
 {	
-	// Add week selector	
-	var selectedItems = [];		// Holds all selectable elements which are already selected
-	var numSelecting = 0;		// Keep count of how many elements we have selected during selecting event
-	var selectAll = false;
-	var removeAll = false;
-	
-	$("#popupWeeks").bind("mousedown", function(e) {				
-		e.metaKey = true;		// Simulates holding down control to select non-adjacent elements
-	}).selectable(
-	{	
-		selecting: function(e, ui)
-		{
-			var clickedText = ui.selecting.innerHTML;
-			
-			if (numSelecting == 0)		// If this is the first element being selected
-			{
-				// If element is not already selected, we now add all other elements we later select
-				if (selectedItems.indexOf(clickedText) == -1)
+	// Add week selector
+	$(function() {
+		var startElementSelected = false;
+		
+		$("#popupWeeks").selectable({
+			start: function(e){		// Add more to list if current one dragging is not selected, otherwise remove it
+				/*var classSelected = e.toElement.className.search("ui-selected");
+				if (classSelected != -1)	// clicked element is already selected
 				{
-					selectAll = true;
-					removeAll = false;
+					startElementSelected = true;
 				}
-				else	// If element is already selected, then we remove all other elements we later select
+				else
 				{
-					selectAll = false;
-					removeAll = true;
-				}
-			}						
-			
-			// If element is not selected, add it to array
-			if (selectAll)
-			{
-				selectedItems.push(clickedText);
-			}
-			
-			if (removeAll)	// If element is already selected, then remove it from the array and remove it's class
-			{
-				selectedItems.splice(selectedItems.indexOf(clickedText), 1);
-				ui.selecting.className = "ui-state-default";
-			}
+					startElementSelected = false;
+				}	*/
 
-			// Move on to the next element
-			numSelecting++;
-		},
-		
-		stop: function()
-		{
-			// Reset selectedItems and numSelecting to 0
-			selectedItems.length = 0;
-			numSelecting = 0;
-			var selectAll = false;
-			var removeAll = false;
-			
-			// For every selected element, add it to the array
-			$(".ui-selected", this).each(function() 
+				$("#popupWeeks").on("click",function(){
+					alert("The paragraph was clicked.");
+				});
+			},
+			selecting: function(e, ui)
 			{
-				selectedItems.push(this.innerHTML);
-			});
-		},
-		
-		distance: 1			// This is so we can register normal mouse click events
-	});
-	
-	// Since the distance on the selectable is now greater than zero, clicks do not work if the mouse does not move
-	// Simulate mouse click like the selector would normally
-	$("#popupWeeks li").click(function()
-	{
-		var clickedText = $(this).text();
-		
-		// If element is not selected, add it to array and add selected class
-		if (selectedItems.indexOf(clickedText) == -1)
-		{						
-			$(this).addClass('ui-selected');
-			selectedItems.push(clickedText);
-		}
-		else 	// If element is already selected, then remove it from the array and remove selected class
-		{
-			$(this).removeClass('ui-selected');
-			selectedItems.splice(selectedItems.indexOf(clickedText), 1);
-		}
+				if (startElementSelected == true)
+				{
+					ui.selecting.className = "ui-state-default";
+				}
+				else
+				{
+					
+				}
+			}
+		});	
 	});
 	
 	// Add park selector
@@ -118,7 +70,7 @@ $(document).ready(function()		// Execute all of this on load
 		$('#facilitiesDiv').html(data);
 	});
 	
-	// Load Parts
+		// Load Parts
 	$(function()
 	{
 		var deptCode = "deptCode=" + document.getElementById("deptDiv").innerHTML;
