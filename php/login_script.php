4e0b5586280974fcd8a3f1d8be5ee6dca7dfa8c2
Prakash Waghela <p.waghela-12@student.lboro.ac.uk>
2015-02-03 13:40:12 +0000

    renamed login script and moved to php dir

--- php/login_script.php
+++ php/login_script.php
@@ -24,30 +24,9 @@
 	{
 		while ($row = $res->fetchRow())
 		{
-			$deptCode = $row["deptcode"];
-			
-			$url = "http://co-project.lboro.ac.uk/team11/Prakash/homepage.php";
-			$fields = array
-			(
-				'deptCode' => $deptCode
-			);
-			$postvars = http_build_query($fields);
-			
-			// build the urlencoded data
-			$postvars = http_build_query($fields);
-
-			// open connection
-			$ch = curl_init();
-
-			// set the url, number of POST vars, POST data
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_POST, count($fields));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-			// execute post
-			$result = curl_exec($ch);
-
-			// close connection
-			curl_close($ch);
+			session_start();
+			$_SESSION["deptCode"] = $row["deptcode"];
+			header("location:../homepage.php");
 		}
 	}
 ?>
