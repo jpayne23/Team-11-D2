e1a9792cd9b81609c617da2a6c58878b39a0a53a
jpayne23 <payne_500@hotmail.com>
2015-02-15 03:03:02 +0000

    next round allocates rooms on successful requests :+1:
    No checking of clashes though but its 3am and we wanna go home.

--- php/simulateRound.php
+++ php/simulateRound.php
@@ -22,7 +22,7 @@
 		$userID = $row["userid"];
 	}
 	
-	$sql="SELECT RequestID FROM Request WHERE UserID = ".$userID." AND Status = 'Submitted';";
+	$sql="SELECT RequestID FROM Request WHERE UserID = ".$userID." AND Status = 'Submitted' AND AdhocRequest = '0'";
 	$res =& $db->query($sql);
 	if(PEAR::isError($res))
 	{
@@ -34,17 +34,55 @@
 		if ($passfail>=0.5)
 		{
 			$sql2 = "UPDATE Request SET Status = 'Successful' WHERE RequestID = ".$row['requestid'].";";
+			
 			$res2 =& $db->query($sql2);
 			if(PEAR::isError($res2))
 			{
 				die($res2->getMessage());
 			}
+		
+			$rooms = array();
+			$dayID = 0;
+			$periodID = 0;
+			
+			$sql3 = "SELECT Room , DayID, PeriodID FROM Request JOIN RequestToRoom ON RequestToRoom.RequestID = Request.RequestID ";
+			$sql3 .= "JOIN RoomRequest ON RoomRequest.RoomRequestID = RequestToRoom.RoomRequestID ";
+			$sql3 .= "WHERE Request.RequestID = '".$row['requestid']."'";
+			
+			
+			$res3 =& $db->query($sql3);
+			if(PEAR::isError($res3))
+			{
+				die($res3->getMessage());
+			}
+			
+			if ($res3->numRows() > 0)
+			{
+				while ($row3 = $res3->fetchRow())
+				{
+					$rooms[count($rooms)] = $row3['room'];
+					$dayID = $row3['dayid'];
+					$periodID = $row3['periodid'];
+					
+				}
+				
+				for ($i=0;$i<count($rooms);$i++)
+				{
+					$sql4 = "INSERT INTO AllocatedRooms (RequestID,Room,DayID,PeriodID) VALUES ('". $row['requestid'] ."','" . $rooms[$i] . "','" . $dayID . "','" . $periodID . "');";
+					
+					$res4 =& $db->query($sql4);
+					if(PEAR::isError($res4))
+					{
+						die($res4->getMessage());
+					}
+				}
+			}
 		}
 		else
 		{
-			$sql2 = "UPDATE Request SET Status = 'Unsuccessful' WHERE RequestID = ".$row['requestid'].";";
+			$sql2 = "UPDATE Request SET Status = 'Unsuccessful' WHERE RequestID = ".$row['requestid']."";
 			$res2 =& $db->query($sql2);
-			if(PEAR::isError($re2s))
+			if(PEAR::isError($res2))
 			{
 				die($res2->getMessage());
 			}
