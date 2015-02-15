325958c1414dffed928b82702c65cddcc7c889a8
Bhavnit Patel <bhavnitrulez291@hotmail.com>
2015-02-15 17:55:47 +0000

    More polishes on multi-room request

--- javascript/requestscript.js
+++ javascript/requestscript.js
@@ -104,7 +104,7 @@ function updateAdvancedBuilding(value) //function to display the list of buildin
 		$("#parkcontent").append(data);
 		var buildingName =  value.substr(4,1).toUpperCase() + value.substring(5,value.length);
 		//add the option of any building
-		$('#parkcontent').children().eq(1).after('<table class= "anycontenttable"> <tr id="choice'+value+'" class="anycontentrows" onclick="addAnyBuilding(this.id); clearBuildingContent(); addRoomTitle()"><td>Any '+buildingName+' Building</td></tr><tr><td></br></td></tr></table>');
+		$('#parkcontent').children().eq(1).after('<table class= "anycontenttable"> <tr id="choice'+value+'" class="anycontentrows" onclick="addAnyPark(this.id); clearBuildingContent(); addRoomTitle()"><td>Any '+buildingName+' Building</td></tr><tr><td></br></td></tr></table>');
 		length = $('#parkcontent').children().length -1;
 		lastRow = $('#parkcontent').children().eq(length);
 		lastRow.attr('style', 'border-bottom: 0');
@@ -532,11 +532,21 @@ function addAnyPark(id)
 	maxCap = parseInt(document.getElementById("maxGroupSize").value);
 	reqCap = parseInt(document.getElementById("groupSize").value);
 	
+	console.log(id);
+	var str = 'rm'+id;
+	var len = $('#chosenRooms').children().children().length;
+	for(i=0;i<len;i++){
+		var parkname = $('#chosenRooms').children().children().eq(i).attr('id');
+		if(str == parkname)
+			return;
+		
+	}
+	
 	if(reqCap > 0){
 		
 		var parkname = id.substr(6);
 		parkname = parkname.substr(4).toUpperCase();
-		console.log(parkname);
+		//console.log(parkname);
 		var html= "<tr id="+("rm" + id)+"><td id ='cap"+id+"'>"+reqCap+"</td><td> Students in park </td><td>"+parkname+"</td><td id='del"+ ("rm" + id) +"' onclick='deleteRoom(this.id);'><img src='img/delete.png' height='15' width='15'><td></tr>";
 		$("#selectedrooms").append(html);
 		document.getElementById('chosenRooms').innerHTML += html;
@@ -564,18 +574,3 @@ function findRoomOpenClose()
 
 	
 
-
-function addAnyBuilding(id)
-{
-	maxCap = parseInt(document.getElementById("maxGroupSize").value);
-	reqCap = parseInt(document.getElementById("groupSize").value);
-	
-		if(reqCap > 0){
-		
-		var buildingname = $('#buildingcontent').children().eq(4).attr('data-building');
-		var html= "<tr id="+("rm" + id)+"><td id ='cap"+id+"'>"+reqCap+"</td><td> Students in </td><td>"+buildingname+"</td><td id='del"+ ("rm" + id) +"' onclick='deleteRoom(this.id);'><img src='img/delete.png' height='15' width='15'><td></tr>";
-		$("#selectedrooms").append(html);
-		document.getElementById('chosenRooms').innerHTML += html;
-	}
-	
-}
