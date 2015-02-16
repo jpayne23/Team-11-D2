9415f1f8126944bc8f83e6edb393a938a25c8b2f
Bhavnit Patel <bhavnitrulez291@hotmail.com>
2015-02-16 14:39:11 +0000

    Commented these files (They need wrapping)
    LoadModCodes
    loadTimesOfRoom

--- php/loadModCodes.php
+++ php/loadModCodes.php
@@ -1,4 +1,11 @@
 <?php
+/*This script is called from script.js --> updateModCode() and its purpose is to
+update the drop down list on the main page with the list of modules available
+in the database.
+
+Contributions by Prakash 
+*/
+
 	// Setting up connecting to the database
 	require_once 'MDB2.php';			
 	include "/disks/diskh/teams/team11/passwords/password.php";
@@ -9,18 +16,20 @@
 	}
 	$db->setFetchMode(MDB2_FETCHMODE_ASSOC);
 	
+	//input the department code and part
 	$deptCode = $_REQUEST['deptCode'];
 	$part = $_REQUEST['part'];
 	
-	if ($part == "any")
+	if ($part == "any") //get all modcodes if part = any
 		$sql = "SELECT ModCode, Title FROM Module WHERE DeptCode = '".$deptCode."';";	
-	else
+	else //get all modcodes from the user chosen part
 		$sql = "SELECT ModCode, Title FROM Module WHERE DeptCode = '".$deptCode."' and Part = '".$part."';";
 	$res =& $db->query($sql);
 	if(PEAR::isError($res))
 	{
-		die($res->getMessage());
+		die($res->getMessage()); //give error message
 	}
+	//output html which is a list of module codes and names
 	echo '<select class= "optionResize" name="modCode" title="Module Codes and Titles" id="modCodes" onclick="resetSelectedRooms();" style="width: 350px;">';
 	while ($row = $res->fetchRow())
 	{
--- php/loadTimesOfRoom.php
+++ php/loadTimesOfRoom.php
@@ -1,4 +1,11 @@
 <?php
+/*
+This script is called from requestscript.js --> fillTimetable() and its purpose
+is to return a list of requests that have booked a room, including their days
+and times and weeks booked.
+
+Contributions by Bhavnit
+*/
 	// Setting up connecting to the database
 	require_once 'MDB2.php';			
 	include "/disks/diskh/teams/team11/passwords/password.php";
@@ -9,12 +16,9 @@
 	}
 	$db->setFetchMode(MDB2_FETCHMODE_ASSOC);
 	
-	$roomNo = $_REQUEST['roomNo'];
+	$roomNo = $_REQUEST['roomNo']; //Bring in the chosen room number
 
-	/*$sql = "SELECT RequestIDHist, DayID, PeriodID, SessionLength FROM `RequestHist` where RequestIDHist in
-			(select RequestIDHist from RequestToRoomHist where RoomRequestIDHist in
-			(select RoomRequestIDHist from RoomRequestHist where Room = '".$roomNo."'));";
-	*/
+	//query to get the list of requests that match the room
 	$sql= "SELECT a.RequestID, a.DayID, a.PeriodID, SessionLength FROM AllocatedRooms a join Request on Request.RequestID = a.RequestID where a.Room = '".$roomNo."'";
 			
 			
@@ -24,6 +28,8 @@
 		die($res->getMessage());
 	}
 	
+	//query to use each request from the above query and find the list of weeks
+	//that it has been booked for.
 	$sql = "select WeekRequest.RequestID, Weeks from WeekRequest where WeekRequest.RequestID in(
 			SELECT a.RequestID FROM AllocatedRooms a join Request on Request.RequestID = a.RequestID where a.Room = '".$roomNo."')";
 			
@@ -33,9 +39,10 @@
 	$res2 =& $db->query($sql);
 	if(PEAR::isError($res2))
 	{
-		die($res2->getMessage());
+		die($res2->getMessage()); //output the error message and end script
 	}
 	
+	//local arrays for the booked weeks
 	$a = array();
 	$ar = array();
 	$arr = array();
@@ -46,17 +53,19 @@
 		$a[sizeof($a)] = $row["weeks"];
 	}
 	
-	while ($row = $res->fetchRow())
+	while ($row = $res->fetchRow()) //loop through each result
 	{
 		$weeks = "";
 		for($i = 0;$i<sizeof($a);$i=$i+2)
 		{
 			if($a[$i] == $row["requestid"]){
-				$weeks .= ":".$a[$i+1];
+				$weeks .= ":".$a[$i+1]; //append the weeks with a colon
 			}
 		}
+		//create the id to be used in javascript
 		$id = 'd'.$row["dayid"].'p'.$row["periodid"];
 		
+		//assign the attributes of the associative array
 		$ar['id'] = $id;
 		$ar['data-selected'] = '1';
 		$ar['data-length'] = $row["sessionlength"];
@@ -64,8 +73,8 @@
 		$ar['data-available'] = '';
 		$ar['data-display'] = '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]';
 		$ar['class'] = 'timeslotbooked';
-		$arr[sizeof($arr)] = $ar;
-		//echo "<td id='".$id."' data-selected='1' data-length='".$row["sessionlength"]."' data-weeks='".$weeks."' data-available='' class='timeslotbooked'>Available</td>,,"; 
+		$arr[sizeof($arr)] = $ar; //push the contents to the array
 	}
-	echo json_encode($arr);
+	echo json_encode($arr); //return the encoded array to be used for the 
+	//timetable
 ?>
